f50c00a94616865c262fe0c4aa13a40a7eb185a1
Iuro Nascimento <iurobpn@proton.me>
2026-01-26 08:36:15 -0300

    add husky simulation to launch script

--- shared/launch_gz.sh
+++ shared/launch_gz.sh
@@ -3,15 +3,17 @@
 set -e
 
 cd /mnt/shared/sim_quad/cpp/build/Debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
+# cmake -DCMAKE_BUILD_TYPE=Debug ../..
 ninja -j4
 sudo ninja install
 sudo ldconfig
 
+source /opt/ros/noetic/setup.bash
+
 cd /mnt/shared/catkin_ws
 catkin_make
 
-source /opt/ros/noetic/setup.bash
 source devel/setup.bash
 
-roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/quad_10obs.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/quadcopter/config/config.xml
+# roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/quad_20obs.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/quadcopter/config/config.xml
+roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/husky_1obs/husky_rnd.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/husky2/config/config.xml
