0af50a2c3200a7115283c959befda39850a9c709
Iuro Nascimento <iurobpn@proton.me>
2026-02-27 17:15:49 -0300

    update launch script to add color scenarios

--- shared/launch_gz.sh
+++ shared/launch_gz.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd /mnt/shared/sim_quad/cpp/build/Debug
+cd /mnt/shared/sim_quad/cpp/build/Release
 # cmake -DCMAKE_BUILD_TYPE=Debug ../..
 ninja -j4
 sudo ninja install
@@ -16,4 +16,4 @@ catkin_make
 source devel/setup.bash
 
 # roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/quad_20obs.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/quadcopter/config/config.xml
-roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/husky_1obs/husky_rnd.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/husky2/config/config.xml
+roslaunch Database gazebo.launch world:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/worlds/worlds/iuro/husky_1obs/husky_rnd_color.world control_strategy:=/mnt/shared/catkin_ws/src/ProVANT-Simulator_Developer/source/Database/models/husky2/config/config.xml
